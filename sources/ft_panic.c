68437cf0bfaa5692aa6f47a7c142cd6ba61195cd
Kenneth Cheong <kennethkhc29@gmail.com>
2026-04-09 20:35:45 +0800

    feat: add ft_panic

---
+++ sources/ft_panic.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_panic.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kecheong <kecheong@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/06 20:06:18 by kecheong          #+#    #+#             */
+/*   Updated: 2026/04/06 20:37:19 by kecheong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_dprintf.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+void	ft_panic(int status, const char *format, ...)
+{
+	va_list	args;
+
+	va_start(args, format);
+	ft_vdprintf(STDERR_FILENO, format, args);
+	va_end(args);
+	exit(status);
+}
