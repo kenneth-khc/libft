6cb9a47f65776070e1a085e4da7167cfe4fa7536
Kenneth Cheong <kennethkhc29@gmail.com>
2026-04-09 20:27:22 +0800

    build: add flake.nix and flake.lock

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "libft development shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = with pkgs; {
+          default = mkShell {
+            packages = [
+              clang
+              clang-tools
+              gnumake
+              gdb
+              bear
+              bash
+              entr
+              norminette
+            ];
+            hardeningDisable = [ "all" ];
+          };
+        };
+      }
+    );
+}
